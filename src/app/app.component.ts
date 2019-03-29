a9fc94cc829b0f8f1162bb9f2856c5b0c778b864
cs.mccaleb@gmail.com <cs.mccaleb@gmail.com>
2019-03-28 21:09:46 -0500

    Programmatic post generation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,70 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'uonai';
+  posts = [
+    {
+    'id': 1,
+    'title': 'Uppakoma',
+    'subTitle': 'A localized events platform (Technologies: ReactNative, .NET, MongoDB, Azure Cloud)',
+    'technologies': [
+      {
+        id: 1,
+        technology: 'ReactNative'
+      },
+      {
+        id: 2,
+        technology: '.NET'
+      },
+      {
+        id: 3,
+        technology: 'MongoDB'
+      },
+    ]
+  },
+  {
+    'id': 2,
+    'title': 'Hydra',
+    'subTitle': 'An IoT cloud platform (Technologies: Angular, .NET, Azure Cloud)',
+    'technologies': [
+      {
+        id: 1,
+        technology: 'ReactNative'
+      },
+      {
+        id: 2,
+        technology: '.NET'
+      },
+      {
+        id: 3,
+        technology: 'MongoDB'
+      },
+    ]
+  },
+  {
+    'id': 3,
+    'title': 'Vitae',
+    'subTitle': 'A dashboard for showing real-time fitness gains (Technologies: Angular, .NET, SQL Server, Azure Cloud)',
+    'technologies': [
+      {
+        id: 1,
+        technology: 'ReactNative'
+      },
+      {
+        id: 2,
+        technology: '.NET'
+      },
+      {
+        id: 3,
+        technology: 'MongoDB'
+      },
+    ]
+  }
+
+
+
+];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatListModule } from '@angular/material';
+import { MatButtonModule, MatListModule, MatExpansionModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -12,7 +12,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatListModule
+    MatExpansionModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
